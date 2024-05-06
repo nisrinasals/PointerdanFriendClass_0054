3b98aaf8a10ea83fb5a1dac2ff31393965fc3b1e
nisrinasals <nisrinahikmawan@gmail.com>
2024-05-06 15:23:47 +0700

    membuat kelas siswa dan orang pada frienClassSatu

---
+++ friendClassSatu.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+private:
+    int id;
+public: 
+    void setId(int pId);
+    void displayAll(orang &a);
+};
+
